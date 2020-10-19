b36ca9104e950eaaa16ab0002e05b0c85eed3788
David_Velez <velezdavid6@gmail.com>
2020-10-19 17:14:36 -0400

    final projects section design implementation

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,14 +1,57 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import './ProjectCard.css'
-import trees from '../images/trees.jpg'
 
-const ProjectCard = ({number, title, technology1, technology2, technology3, technology4}) => {
+const ProjectCard = ({
+  number,
+  title,
+  technology1,
+  technology2,
+  technology3,
+  technology4,
+  technology5,
+  technology6,
+  description,
+  image }) => {
+
+    const bottomStyle = {
+      display: 'flex',
+      flexDirection: 'column',
+      height: '70%',
+      width: '100%',
+      backgroundImage: `url(${image})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat'
+    }
+
+    const descriptionStyle = {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      textAlign: 'center',
+      fontSize: '1.25rem',
+      height: '85%',
+      width: '100%',
+      fontWeight: 'bold'
+      // backgroundColor: 'orange'
+    }
+
+    const demoStyle = {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-around',
+      height: '15%',
+      width: '100%',
+      // backgroundColor: 'blue',
+    }
 
 
     return (
     <div className='single-project'>
       <div className='single-project-top'>
-        
+
         <div className='project-description-container'>
           <div className='project-title'>
             <h1>{title}</h1>
@@ -19,17 +62,27 @@ const ProjectCard = ({number, title, technology1, technology2, technology3, tech
             </div>
             <div className='technologies-used-bottom'>
               <div className='technologies-used-container '>
-            <h3>{technology1}</h3>
-            <h3>{technology2}</h3>
-            <h3>{technology3}</h3>
-            <h3>{technology4}</h3>
+            <h3 style={{color: '#0984e3'}}>{technology1}</h3>
+            <h3 style={{color: '#deac70'}}>{technology2}</h3>
+            <h3 style={{color: '#b007a8'}}>{technology3}</h3>
+            <h3 style={{color: '#2d3436'}}>{technology4}</h3>
+            <h3>{technology5}</h3>
+            <h3 style={{color: '#0984e3'}}>{technology6}</h3>
               </div>
 
             </div>
           </div>
         </div>
       </div>
-      <div className='single-project-bottom'></div>
+      <div style={bottomStyle}>
+        <div style={descriptionStyle}>
+          <p>{description}</p>
+        </div>
+        <div style={demoStyle}>
+          <button type="button">Click Me!</button>
+          <button type="button">Click Me!</button>
+        </div>
+      </div>
     </div>
   )
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,10 +4,12 @@ import Card from './Card'
 import ProjectCard from './ProjectCard'
 
 import project from '../images/project.svg'
-import trees from '../images/trees.jpg'
+import simon from '../images/simon.jpg'
 import cotrip from '../images/cotrip.png'
 import barbershop from '../images/barbershop.svg'
 import toronto from '../images/toronto.png'
+import portfolio from '../images/portfolio.svg'
+import ecommerce from '../images/ecommerce.png'
 
 
 
@@ -17,6 +19,11 @@ export default class Projects extends Component {
 
     this.state = {
       id: 0,
+      showFirst: false,
+      showSecond: false,
+      showThird: false,
+      showFourth: false,
+      showFifth: false
     }
   }
 
@@ -25,75 +32,145 @@ export default class Projects extends Component {
     this.setState({
       id: e.target.id
     })
-    // console.log(this.state.id)
   }
 
+  showItems = () => {
+    let num = 0
+    setInterval(() => {
+      if (num === 1) {
+        this.setState({
+          showFirst: true
+        })
+      } else if (num === 2) {
+        this.setState({
+          showSecond: true
+        })
+      } else if (num === 3) {
+        this.setState({
+          showThird: true
+        })
+      } else if (num === 4) {
+        this.setState({
+          showFourth: true
+        })
+      } else if (num === 5) {
+        this.setState({
+          showFifth: true
+        })
+      } else if (num === 6) {
+        this.setState({
+          showFifth: false
+        })
+      } else if (num === 7) {
+        this.setState({
+          showFourth: false
+        })
+      } else if (num === 8) {
+        this.setState({
+          showThird: false
+        })
+      } else if (num === 9) {
+        this.setState({
+          showSecond: false
+        })
+      } else if (num === 10) {
+        this.setState({
+          showFirst: false
+        })
+        num = 0
+      }
+      num++
+    }, 800)
+  }
 
+  componentDidMount(){
+    this.showItems()
+  }
 
   render(){
-      let card = <div style={{flexDirection: 'row', flexWrap: 'wrap'}} className='projects-details'>
-        <h1>Work on This!</h1>
+
+      let card = <div style={divStyle1}>
+      <div style={{opacity: '0'}} className='projects-image-parent'></div>
+        <div style={{backgroundImage: `url(${barbershop})`, opacity: this.state.showFirst ? '1' : '0'}} className='projects-image-first-child'>
+          <div style={{backgroundImage: `url(${toronto})`, opacity: this.state.showSecond ? '1' : '0'}} className='projects-image-second-child'>
+            <div style={{backgroundImage: `url(${simon})`, opacity: this.state.showThird ? '1' : '0'}} className='projects-image-third-child'>
+              <div style={{backgroundImage: `url(${ecommerce})`, opacity: this.state.showFourth ? '1' : '0'}} className='projects-image-fourth-child'>
+                <div style={{backgroundImage: `url(${portfolio})`, opacity: this.state.showFifth ? '1' : '0'}} className='projects-image-fifth-child'></div>
+              </div>
+            </div>
+          </div>
+      </div>
+
       </div>
       if (this.state.id == 1) {
-        card = <div className='projects-details'>
+        card = <div style ={divStyle2}>
         <ProjectCard
+        image={barbershop}
         number={2}
         title={'AZ Hair Studio Website'}
-        technology1={'Python'}
-        technology2={'Flask'}
-        technology3={'PostgreSQL'}
-        technology4={'PeeWee'}
+        description={'This is where json stuff will go or graphQL'}
+        technology1={'HTML5'}
+        technology2={'CSS'}
+        technology3={'Javascript'}
+        technology4={'REACT'}
+        technology5={'Gatsby'}
+        technology6={'Netlify'}
         />
         </div>
       }
       else if (this.state.id == 2) {
-      card = <div className='projects-details'>
+      card = <div style ={divStyle2}>
       <ProjectCard
+      image={toronto}
       number={2}
       title={'Toronto Website'}
-      technology1={'Python'}
-      technology2={'Flask'}
-      technology3={'PostgreSQL'}
-      technology4={'PeeWee'}
+      technology1={'HTML5'}
+      technology2={'CSS'}
+      technology3={'Javascript'}
       />
       </div>
     } else if (this.state.id == 3) {
-        card = <div className='projects-details'>
+        card = <div style ={divStyle2}>
         <ProjectCard
+        image={simon}
         number={3}
-        title={'Kush API'}
-        technology1={'Python'}
-        technology2={'Flask'}
-        technology3={'PostgreSQL'}
-        technology4={'PeeWee'}
+        title={'Simon'}
+        technology1={'HTML5'}
+        technology2={'CSS'}
+        technology3={'Javascript'}
+        technology4={'REACT'}
         />
         </div>
     } else if (this.state.id == 4) {
-      card = <div className='projects-details'>
+      card = <div style ={divStyle2}>
       <ProjectCard
+      image={ecommerce}
       number={3}
       title={'MERN E-Commerce Site'}
-      technology1={'Python'}
-      technology2={'Flask'}
-      technology3={'PostgreSQL'}
-      technology4={'PeeWee'}
+      technology1={'MongoDB'}
+      technology2={'Express.js'}
+      technology3={'REACT'}
+      technology4={'Node.js'}
       />
       </div>
     } else if (this.state.id == 5) {
-      card = <div className='projects-details'>
+      card = <div style ={divStyle2}>
       <ProjectCard
+      image={portfolio}
       number={3}
       title={'Portfolio Site'}
-      technology1={'Python'}
-      technology2={'Flask'}
-      technology3={'PostgreSQL'}
-      technology4={'PeeWee'}
+      technology1={'HTML5'}
+      technology2={'CSS'}
+      technology3={'Javascript'}
+      technology4={'REACT'}
+      technology5={'Gatsby'}
       />
       </div>
     }
 
       return (
         <div id='projects-section' className='projects-container'>
+
           {card}
 
           <div className='projects'>
@@ -102,56 +179,66 @@ export default class Projects extends Component {
             <hr/>
             <p>Some of the recent projects I've worked on</p>
             <Card
-            number={1}
+                number={0}
                 click={this.handleClick}
                 border={'1px solid #32ab9c'}
-                color={'#fff'}
-                defaultBackground={'#32ab9c'}
-                height={'50px'}
+                color={this.state.id == 0 ? '#2d3436' : '#fff'}
+                defaultBackground={this.state.id == 0 ? '#fff' : '#32ab9c'}
+                height={'40px'}
+                width={'80%'}
+                shadow={'5px 10px 20px rgba(0,0,0,0.4)'}
+                skill='** All Projects **' />
+            <Card
+                number={1}
+                click={this.handleClick}
+                border={'1px solid #32ab9c'}
+                color={this.state.id == 1 ? '#2d3436' : '#fff'}
+                defaultBackground={this.state.id == 1 ? '#fff' : '#32ab9c'}
+                height={'40px'}
                 width={'80%'}
                 shadow={'5px 10px 20px rgba(0,0,0,0.4)'}
                 skill='AZ Hair Studio Website' />
 
                 <Card
-                number={2}
+                    number={2}
                     click={this.handleClick}
                     border={'1px solid #32ab9c'}
-                    color={'#fff'}
-                    defaultBackground={'#32ab9c'}
-                    height={'50px'}
+                    color={this.state.id == 2 ? '#2d3436' : '#fff'}
+                    defaultBackground={this.state.id == 2 ? '#fff' : '#32ab9c'}
+                    height={'40px'}
                     width={'80%'}
                     shadow={'5px 10px 20px rgba(0,0,0,0.4)'}
                     skill='Toronto Website' />
 
                     <Card
-                    number={3}
+                        number={3}
                         click={this.handleClick}
                         border={'1px solid #32ab9c'}
-                        color={'#fff'}
-                        defaultBackground={'#32ab9c'}
-                        height={'50px'}
+                        color={this.state.id == 3 ? '#2d3436' : '#fff'}
+                        defaultBackground={this.state.id == 3 ? '#fff' : '#32ab9c'}
+                        height={'40px'}
                         width={'80%'}
                         shadow={'5px 10px 20px rgba(0,0,0,0.4)'}
-                        skill='Kush API' />
+                        skill='Simon' />
 
                         <Card
-                        number={4}
+                            number={4}
                             click={this.handleClick}
                             border={'1px solid #32ab9c'}
-                            color={'#fff'}
-                            defaultBackground={'#32ab9c'}
-                            height={'50px'}
+                            color={this.state.id == 4 ? '#2d3436' : '#fff'}
+                            defaultBackground={this.state.id == 4 ? '#fff' : '#32ab9c'}
+                            height={'40px'}
                             width={'80%'}
                             shadow={'5px 10px 20px rgba(0,0,0,0.4)'}
                             skill='MERN E-Commerce' />
 
                             <Card
-                            number={5}
+                                number={5}
                                 click={this.handleClick}
                                 border={'1px solid #32ab9c'}
-                                color={'#fff'}
-                                defaultBackground={'#32ab9c'}
-                                height={'50px'}
+                                color={this.state.id == 5 ? '#2d3436' : '#fff'}
+                                defaultBackground={this.state.id == 5 ? '#fff' : '#32ab9c'}
+                                height={'40px'}
                                 width={'80%'}
                                 shadow={'5px 10px 20px rgba(0,0,0,0.4)'}
                                 skill='Portfolio Site' />
@@ -159,4 +246,18 @@ export default class Projects extends Component {
         </div>
       )
   }
+}
+
+const divStyle1 = {
+  width: '50%',
+  height: '85%'
+}
+
+const divStyle2 = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '50%',
+  height: '85%'
 }
