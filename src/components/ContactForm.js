9173e7c03a7490fc1fa20614451b146ec1d828e8
David_Velez <velezdavid6@gmail.com>
2020-11-27 16:37:51 -0500

    messing with form functionality

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -39,7 +39,7 @@ const ContactForm = () => {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact-form", fullName })
+      body: encode({ "form-name": "contact-form", ...fullName })
     })
       .then(() => alert("Success!"))
       .catch(error => alert(error));
