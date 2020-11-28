974f1b124973e818e3fb594f3f0e08bdb343f5cb
David_Velez <velezdavid6@gmail.com>
2020-11-27 19:40:29 -0500

    form issuse

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ContactForm.css'
 
 
@@ -8,45 +8,101 @@ const encode = (data) => {
         .join("&");
   }
 
-  class ContactForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { name: "", email: "", message: "" };
-    }
+const ContactForm = () => {
+  const [fullName, setFullName] = useState({
+    fName: '',
+    lName: '',
+    email: '',
+    message: ''
+  })
 
-    /* Here’s the juicy bit for posting the form submission */
+  const handleChange = (e) => {
+    const { value, name } = e.target
 
-    handleSubmit = e => {
-      fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: encode({ "form-name": "contact-form", ...this.state })
-      })
-        .then(() => alert("Success!"))
-        .catch(error => alert(error));
+    setFullName((prevValue) => {
+      if (name === 'fName') {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email: prevValue.email,
+          message: prevValue.message
+        }
+      } else if (name === 'lName') {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+          email: prevValue.email,
+          message: prevValue.message
+        }
+      } else if (name === 'email-inputted') {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value,
+          message: prevValue.message
+        }
+      } else if (name === 'message') {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: prevValue.email,
+          message: value
+        }
+      }
+    })
 
-      e.preventDefault();
-    };
-
-    handleChange = e => this.setState({ [e.target.name]: e.target.value });
-
-    render() {
-      const { name, email, message } = this.state;
-      return (
-        <div id='contact-form'>
-        <form onSubmit={this.handleSubmit}>
-          <input placeholder='Full Name' type="text" name="name" value={name} onChange={this.handleChange} />
-
-          <input placeholder='Email' type="email" name="email" value={email} onChange={this.handleChange} />
-
-          <textarea placeholder='Message' name="message" value={message} onChange={this.handleChange} />
-
-          <button type="submit">Send</button>
-
-        </form>
-        </div>
-      );
-    }
   }
 
-  export default ContactForm
+  const handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...fullName })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+
+
+  return (
+    <div id='contact-form'>
+      <p style={{fontSize: '2rem', margin: '0'}}>Hello {fullName.fName} {fullName.lName}</p>
+      <form
+        onSubmit={handleSubmit}
+        data-netlify='true'
+        data-netlify-honeypot='bot-field'
+      >
+        <input
+          name='fName'
+          placeholder='First Name'
+          value={fullName.fName}
+          onChange={handleChange}
+        />
+        <input
+          name='lName'
+          placeholder='Last Name'
+          value={fullName.lName}
+          onChange={handleChange}
+        />
+        <input
+          name="email-inputted"
+          placeholder="Email"
+          value={fullName.email}
+          onChange={handleChange}
+        />
+        <textarea
+          name='message'
+          placeholder='Message'
+          value={fullName.message}
+          onChange={handleChange}
+        />
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default ContactForm
