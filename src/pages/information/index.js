223e919c3cb518943d6c29322edc407779c16686
David_Velez <velezdavid6@gmail.com>
2020-12-07 13:32:56 -0500

    created information page placeholder

--- src/pages/information/index.js
+++ src/pages/information/index.js
@@ -1,8 +1,28 @@
 import React from 'react'
-import './Information.css'
+import './information.css'
+import SEO from "../../components/seo"
+import Layout from '../../components/layout'
+import PageBreak from '../../components/PageBreak'
+import BlogComponent from '../../components/BlogComponent'
+
 
 const InfoPage = () => (
-  <div className='info-container'></div>
+  <Layout>
+    <SEO title="Information" />
+    <div className='info-container'>
+
+      <div className='info-intro'>
+      <h1>Information</h1>
+      <p>FAQ's and General Information</p>
+      </div>
+
+      <PageBreak />
+
+      <BlogComponent />
+
+    </div>
+
+  </Layout>
 )
 
 export default InfoPage
