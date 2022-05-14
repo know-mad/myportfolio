12ced19da4ae5fb8ede58450918b050d218af94d
David_Velez <velezdavid6@gmail.com>
2022-05-14 10:04:24 -0400

    adjusted faq page

--- src/pages/faq/index.js
+++ src/pages/faq/index.js
@@ -1,45 +1,54 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import './index.css'
-import Layout from '../../components/layout'
-import JSONData from '../../data/faq.json'
+import React from "react"
+import { Link } from "gatsby"
+import "./index.css"
+import Layout from "../../components/layout"
+import JSONData from "../../data/faq.json"
 
 const QuestionsPage = () => {
-    return (
-        <Layout>
-            <div className='faq-page-container'>
-            <section>
-                <div className='faqs'>
-                    <h2>FREQUENTLY ASKED QUESTIONS</h2>
-                    {
-                    JSONData.map((item, index) => {
-                        return <div key={index} className='faq-container'>
-                                <div className='faq-question'>
-                                <p>{index + 1}. <span style={{color: `#b007a8`}}>{item.question}</span></p>
-                                </div>
-                                <div className='faq-answer'>
-                                <p>{item.answer}</p>
-                                </div>
-                            </div>
-                    })
-                    }
-                </div>
-            </section>
-            <section>
-                <div style={{marginBottom: `0`}} className='contact-banner'>
-                <div className='contact-banner-left'>
-                    <p>
-                    Ready to start your web project?
-                    </p>
-                </div>
-                <div className='contact-banner-right'>
-                    <Link style={{width: `25em`}} id='contact-link' to='/contact/'>START YOUR WEB PROJECT</Link>
-                </div>
-                </div>
-            </section>
+  return (
+    <Layout>
+      <div className="faq-page-container">
+        <section>
+          <div className="portfolio-banner">
+            <div className="banner-inner">
+              <h1>QUESTIONS</h1>
             </div>
-        </Layout>
-    )
+          </div>
+        </section>
+        <section>
+          <div className="faqs">
+            {JSONData.map((item, index) => {
+              return (
+                <div key={index} className="faq-container">
+                  <div className="faq-question">
+                    <h2>
+                      {index + 1}.{" "}
+                      <span style={{ color: `#F446EC` }}>{item.question}</span>
+                    </h2>
+                  </div>
+                  <div className="faq-answer">
+                    <p>{item.answer}</p>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </section>
+        <section>
+          <div style={{ marginBottom: `0` }} className="contact-banner">
+            <div className="contact-banner-left">
+              <p>Ready to start your web project?</p>
+            </div>
+            <div className="contact-banner-right">
+              <Link style={{ width: `25em` }} id="contact-link" to="/contact/">
+                START YOUR WEB PROJECT
+              </Link>
+            </div>
+          </div>
+        </section>
+      </div>
+    </Layout>
+  )
 }
 
 export default QuestionsPage
