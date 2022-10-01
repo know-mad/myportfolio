f112c6c14bbe0a7cc41b11c0fcce180f021cbc83
David_Velez <velezdavid6@gmail.com>
2022-10-01 10:10:38 -0400

    working on mobile slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,11 +1,20 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 import './Slider.css'
 import shopifyAlt from "../images/shopify-plus-logo--black.svg"
+import netlify from "../images/netlify-logo.svg"
+import mailchimpAlt from "../images/mailchimp-alt-logo.svg"
+import contentful from "../images/contentful-logo.svg"
+import stripe from "../images/stripe.svg"
+import jam from "../images/jamstack-icon.svg"
 
 const Slider = () => {
+
+
     useEffect(() => {
         const slider = document.querySelector('.mobile-slider'),
-              slides = Array.from(document.querySelectorAll('.partners-card-mobile'));
+              slides = Array.from(document.querySelectorAll('.partners-card-mobile')),
+              dots = Array.from(document.querySelectorAll('.progression-dot'))
+              console.log('These are the dots', dots)
 
         let isDragging = false,
             startPos = 0,
@@ -13,6 +22,9 @@ const Slider = () => {
             prevTranslate = 0,
             animationID = 0,
             currentIndex = 0
+
+        let active = 0
+        dots[active].style.background = '#F446EC';
 
         slides.forEach((slide, index) => {
             const slideImage = slide.querySelector('img');
@@ -26,11 +38,11 @@ const Slider = () => {
         });
 
         // Disabled right click menu
-        window.oncontextmenu = function(e) {
-            e.preventDefault()
-            e.stopPropagation()
-            return false
-        }
+        // window.oncontextmenu = function(e) {
+        //     e.preventDefault()
+        //     e.stopPropagation()
+        //     return false
+        // }
 
         function touchStart(index) {
             return function(event) {
@@ -52,16 +64,21 @@ const Slider = () => {
             const movedBy = currentTranslate - prevTranslate
             if(movedBy < -100 && currentIndex < slides.length - 1) {
                 currentIndex += 1
+                dots[currentIndex].style.background = '#F446EC';
+                dots[currentIndex - 1].style.background = '#4026B6';
+                console.log('The index is', currentIndex)
             }
 
             if(movedBy > 100 && currentIndex > 0) {
                 currentIndex -= 1
+                dots[currentIndex + 1].style.background = '#4026B6';
+                dots[currentIndex].style.background = '#F446EC';
+                console.log('The index blah', currentIndex)
             }
 
             setPositionByIndex()
 
             slider.classList.remove('grabbing')
-            console.log('end')
         }
 
         function touchMove(event) {
@@ -97,19 +114,77 @@ const Slider = () => {
             <div className="mobile-slider">
                 <div className="partners-card-mobile">
                     <div className="partners-card-mobile-inner">
+                        <p className="partners-card-header">e-commerce</p>
                         <img src={shopifyAlt} />
+                        <p className="partners-card-content">
+                            Working closely with Shopify, I'm able to deliver best-in-class
+                            commerce experiences for high-growth Direct To Consumer (DTC)
+                            brands.
+                        </p>
                     </div>
                 </div>
                 <div className="partners-card-mobile">
                     <div className="partners-card-mobile-inner">
-                        <img src={shopifyAlt} />
+                    <p className="partners-card-header">content management</p>
+                    <img src={contentful} />
+                    <p className="partners-card-content">
+                        As a Contentful partner, I create insanley fast e-commerce
+                        experiences by leveraging the power of Headless Content
+                        Management.
+                    </p>
                     </div>
                 </div>
                 <div className="partners-card-mobile">
                     <div className="partners-card-mobile-inner">
-                        <img src={shopifyAlt} />
+                        <p className="partners-card-header">headless architecture</p>
+                        <img src={jam} />
+                        <p className="partners-card-content">
+                            Removing the requirement to manage or run web servers, I create
+                            fast, secure sites and dynamic headless e-commerce websites.
+                        </p>
                     </div>
                 </div>
+                <div className="partners-card-mobile">
+                    <div className="partners-card-mobile-inner">
+                        <p className="partners-card-header">hosting</p>
+                        <img src={netlify} />
+                        <p className="partners-card-content">
+                            Netlify makes CI/CD, deployment and scaled hosting a commodity
+                            and helps enterprises focus on creating great dynamic consumer
+                            experiences.
+                        </p>
+                    </div>
+                </div>
+                <div className="partners-card-mobile">
+                    <div className="partners-card-mobile-inner">
+                        <p className="partners-card-header">payment solutions</p>
+                        <img src={stripe} />
+                        <p className="partners-card-content">
+                            Through my Stripe partnership I'm able to integrate a fast and
+                            lightweight payment processing system for internet businesses of
+                            all sizes.
+                        </p>
+                    </div>
+                </div>
+                <div className="partners-card-mobile">
+                    <div className="partners-card-mobile-inner">
+                        <p className="partners-card-header">email marketing</p>
+                        <img src={mailchimpAlt} />
+                        <p className="partners-card-content">
+                            I partner with Mailchimp to provide revenue-generating email
+                            marketing services, fueling your brand’s growth and retargetting
+                            campaigns.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="progression-container">
+                <div className="progression-dot"></div>
+                <div className="progression-dot"></div>
+                <div className="progression-dot"></div>
+                <div className="progression-dot"></div>
+                <div className="progression-dot"></div>
+                <div className="progression-dot"></div>
             </div>
         </div>
     )
